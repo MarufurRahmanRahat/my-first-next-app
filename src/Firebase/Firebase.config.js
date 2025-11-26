601b494942c3490642bf80d95a92db4e98d52fb8
MarufurRahmanRahat <marufrahat88@gmail.com>
2025-11-26 15:48:20 +0600

    firebase installed and initialized

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCK46t69qe6k3u7C29JtwNg-spVdacvvos",
+  authDomain: "my-first-next-app-ef2e9.firebaseapp.com",
+  projectId: "my-first-next-app-ef2e9",
+  storageBucket: "my-first-next-app-ef2e9.firebasestorage.app",
+  messagingSenderId: "149914323848",
+  appId: "1:149914323848:web:a4d1cf59d1a04de277483d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
